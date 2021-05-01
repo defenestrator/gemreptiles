6127c18a9d53704bd961691c48c2e177c4ccd890
Jeremy Anderson <529446+defenestrator@users.noreply.github.com>
2021-05-01 13:26:05 -0600

    Search cache like a boss

--- app/Http/Livewire/SearchSpecies.php
+++ app/Http/Livewire/SearchSpecies.php
@@ -4,26 +4,36 @@ namespace App\Http\Livewire;
 
 use App\Models\Species;
 use Livewire\Component;
+use Illuminate\Support\Facades\Cache;
+use App\Traits\Sluggable;
 
 class SearchSpecies extends Component
 {
+    use Sluggable;
     public $search = '';
 
     public function render()
     {
         $all = new \Illuminate\Database\Eloquent\Collection;
-        if (strlen($this->search) > 3 ) {
-            $all = $all->merge(Species::where('species', "like", '%' . $this->search . '%')->get());
-            $all = $all->merge(Species::where('species', "like", $this->search . '%')->get());
-            $all = $all->merge(Species::where('species', "like", '%' . $this->search)->get());
-            $all = $all->merge(Species::where('common_name', 'like', $this->search . '%')->get());
-            $all = $all->merge(Species::where('common_name', 'like', '%' . $this->search)->get());
-            $all = $all->merge(Species::where('common_name', 'like', '%' . $this->search . '%')->get());
-            $all = $all->merge(Species::where('subspecies', 'like', $this->search . '%')->get());
-            $all = $all->merge(Species::where('subspecies', 'like', '%' . $this->search)->get());
-            $all = $all->merge(Species::where('subspecies', 'like', '%' . $this->search . '%')->get());
-        }
 
+        if (strlen($this->search) >= 3 ) {
+            $cacheKey = 'search-string-' . $this->createSlug(strtolower($this->search));
+            if (Cache::has($cacheKey)) {
+                $all = Cache::get($cacheKey);
+            } else {
+                $all = $all->merge(Species::where('species', "like", '%' . $this->search . '%')->get());
+                $all = $all->merge(Species::where('species', "like", $this->search . '%')->get());
+                $all = $all->merge(Species::where('species', "like", '%' . $this->search)->get());
+                $all = $all->merge(Species::where('common_name', 'like', $this->search . '%')->get());
+                $all = $all->merge(Species::where('common_name', 'like', '%' . $this->search)->get());
+                $all = $all->merge(Species::where('common_name', 'like', '%' . $this->search . '%')->get());
+                $all = $all->merge(Species::where('subspecies', 'like', $this->search . '%')->get());
+                $all = $all->merge(Species::where('subspecies', 'like', '%' . $this->search)->get());
+                $all = $all->merge(Species::where('subspecies', 'like', '%' . $this->search . '%')->get());
+                Cache::put($cacheKey, $all);
+            }
+        }
         return view('livewire.search-species', ['species' => $all]);
+
     }
 }
