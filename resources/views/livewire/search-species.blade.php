1b6c280390c6437e61f4387b4d9656fb0acd5546
Jeremy Anderson <529446+defenestrator@users.noreply.github.com>
2021-05-02 08:25:32 -0600

    perfection

--- resources/views/livewire/search-species.blade.php
+++ resources/views/livewire/search-species.blade.php
@@ -4,6 +4,8 @@
             Search for Reptiles
             @if(count($species) > 0)
             <span class="text-sm content-center font-sans text-yellow-700"> {{ count($species) . ' results'}}</span>
+            @elseif(count($species) > 0)
+            <span class="text-sm content-center font-sans text-yellow-700"> {{ count($species) . ' result'}}</span>
             @else
             <span class="text-sm content-center font-sans text-yellow-700"> {{$count}} species</span>
             @endif
