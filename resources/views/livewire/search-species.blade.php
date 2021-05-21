cb36cfac80e8117ebe05ac0060df967aeb7580ef
Jeremy Anderson <529446+defenestrator@users.noreply.github.com>
2021-05-21 14:46:08 -0600

    Legitimacy's day parade sale

--- resources/views/livewire/search-species.blade.php
+++ resources/views/livewire/search-species.blade.php
@@ -1,19 +1,6 @@
 <div class="max-w-7xl mx-auto sm:py-6 px-0 sm:px-6">
 
     <div class="p-6 bg-white border-b border-gray-200 sm:rounded shadow-lg">
-        <p>
-            Our species database was built on the work of the
-            <a class="text-green-500" href="http://www.reptile-database.org/" title="The Reptile Database">
-                worldwide reptile species database
-            </a>, which serves as the latest academic consensus on extant reptile species.
-        </p>
-
-            <p class="my-4">The current representation was built using the latest consensus of worldwide herpetologists, via the
-            <a class="text-green-500" href="http://www.reptile-database.org/data/" title="Reptile Species Checklist">
-                reptile species checklist
-            </a>
-            , published December 14th, 2020.
-        </p>
         <h3 class="text-xl font-serif mb-2">
             Search for Reptiles
             @if(count($species) > 0 && count($species) != 1)
@@ -28,6 +15,7 @@
         <input class="rounded-md form-input
             border-green-500 focus:border-green-400"
             style="--tw-ring-color: rgba(59, 125, 43);" id="search-species" wire:model="search" type="text" placeholder="Search species..."/>
+        <button class="rounded bg-transparent py-1 px-2 text-gray-500" onclick="clearSearch()">clear</button>
 
         @if(count($species) > 0)
         <div class="text-left pt-4">
@@ -59,6 +47,19 @@
             </div>
         </div>
         @endif
+        <p class="mt-4 p-2">
+            Our species database was built on the work of the
+            <a class="text-green-500" href="http://www.reptile-database.org/" title="The Reptile Database">
+                worldwide reptile species database
+            </a>, which serves as the latest academic consensus on extant reptile species.
+        </p>
+
+            <p class="my-4">The current representation was built using the latest work of worldwide herpetologists, via the
+            <a class="text-green-500" href="http://www.reptile-database.org/data/" title="Reptile Species Checklist">
+                reptile species checklist
+            </a>
+            , published December 14th, 2020.
+        </p>
     </div>
 
     @if(Route::currentRouteName()  == 'welcome')
@@ -104,10 +105,14 @@
         let searchInput = document.getElementById('search-species')
         searchInput.select()
         searchInput.value = ""
+        function clearSearch() {
+            searchInput.select()
+            searchInput.value = ""
+        }
         var text = terms[Math.floor(Math.random() * terms.length)];
         var l=text.length;
         var current = 0;
-        var time = 120;
+        var time = 100;
         var writeText = function() {
             searchInput.value+=text[current];
             if(current < l-1) {
