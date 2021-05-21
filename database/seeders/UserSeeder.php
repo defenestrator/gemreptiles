9d74783a60071d5d064590a8ee76b4011ae470ec
Jeremy Anderson <529446+defenestrator@users.noreply.github.com>
2021-05-20 21:20:20 -0600

    more migrations shit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $password = Hash::make(Hash::uuid());
+        User::create([
+            'name'  => 'Jeremy Anderson',
+            'email' => 'jeremyblc@gmail.com',
+            'email_verified_at' => '2021-05-21 03:05:06',
+            'password' => $password,
+            ]);
+    }
+}
