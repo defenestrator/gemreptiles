b2103a8cbf3495fd1bd4952242065229a4514a0e
Jeremy Anderson <529446+defenestrator@users.noreply.github.com>
2021-06-05 07:17:36 -0600

    command route?

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('heartbeat', function () {
+    $this->comment('Tick tock the application is not dead I guess.');
+    Log::info("Heartbeat uhhh...beated?");
+})->purpose('Notify Envoyer heartbeat URL');
