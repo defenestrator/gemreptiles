98c46127ca69ee1f2401e0b36c0c32b451167386
Jeremy Anderson <529446+defenestrator@users.noreply.github.com>
2021-06-04 20:38:17 -0600

    Really dumb heartbeat

---
+++ app/Console/Commands/Heartbeat.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Heartbeat extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'heartbeat';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Tick tock the application is not dead I guess.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        return 0;
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use App\Jobs\Heartbeat;
+use App\Commands\Heartbeat;
 
 class Kernel extends ConsoleKernel
 {
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new Heartbeat, 'default')->everyFiveMinutes()->thenPing('http://beats.envoyer.io/heartbeat/3QeUKYWIlWUwzw4');
+        $schedule->command('heartbeat')->everyFiveMinutes()->thenPing('http://beats.envoyer.io/heartbeat/3QeUKYWIlWUwzw4');
     }
 
     /**
