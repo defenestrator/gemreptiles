52279fd61fb7f8bb8619cbf986ed4999ab56e59c
Jeremy Anderson <529446+defenestrator@users.noreply.github.com>
2021-06-04 20:25:23 -0600

    heartbeat

---
+++ app/Jobs/Heartbeat.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class Heartbeat implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $heartbeatUrl ='http://beats.envoyer.io/heartbeat/3QeUKYWIlWUwzw4';
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'GET'
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($heartbeatUrl, false, $context);
+        if ($result === FALSE) { Log::warning("The hearbeat URL appears to be invalid or non-responsive"); }
+    }
+}
