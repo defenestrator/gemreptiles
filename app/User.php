c25c33df4f40b43353b299298bbdd947f867a754
Jeremy Anderson <529446+defenestrator@users.noreply.github.com>
2021-06-14 20:37:40 -0600

    Wow your image handling sucked, Jetstream

--- app/User.php
+++ app/User.php
@@ -9,6 +9,10 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
+use Intervention\Image\Facades\Image;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -17,7 +21,6 @@ class User extends Authenticatable implements MustVerifyEmail
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
-
 
     /**
      * The attributes that are mass assignable.
@@ -59,4 +62,47 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $appends = [
         'profile_photo_url',
     ];
+
+    /**
+     * Update the user's profile photo.
+     *
+     * @param  \Illuminate\Http\UploadedFile  $photo
+     * @return void
+     */
+    public function updateProfilePhoto(UploadedFile $photo)
+    {
+        tap($this->profile_photo_path, function ($previous) use ($photo) {
+
+            $options = [
+                'visibility'    =>  'public',
+                'Cache-Control' =>  'max-age=31540000',
+                'Expires'       =>  now()->addRealDecade()->format('D, d M Y H:i:s T')
+            ];
+
+            $size = 300;
+            $i = Image::make($photo)
+            ->resize($size, $size, function ($constraint) {
+                $constraint->aspectRatio();
+                $constraint->upsize();
+            })
+            ->encode('webp')
+            ->stream();
+
+            $hash = Str::uuid();
+
+            $fileName = $hash . '.webp';
+
+            Storage::disk('s3')->getDriver()->put('/profile-photos/'. $fileName , $i->__toString(), $options);
+
+            $filePath = "/profile-photos/" . $fileName;
+
+            $this->forceFill([
+                'profile_photo_path' => $filePath
+            ])->save();
+
+            if ($previous) {
+                Storage::disk($this->profilePhotoDisk())->delete($previous);
+            }
+        });
+    }
 }
