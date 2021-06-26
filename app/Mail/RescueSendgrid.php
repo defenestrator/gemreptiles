4accb48b5db45c7b3d01691d8d4e3cc07e844dd3
Jeremy Anderson <529446+defenestrator@users.noreply.github.com>
2021-06-25 21:02:04 -0600

    mail

---
+++ app/Mail/RescueSendgrid.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class RescueSendgrid extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.sendgrid.rescue');
+    }
+
+    public function send()
+    {
+        Mail::send('emails.sendgrid.rescue', [], function($message) {
+        $message->to('sendgridtesting@gmail.com')->subject('927EB6CE28901B26');
+});
+    }
+}
